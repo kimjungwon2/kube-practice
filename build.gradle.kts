d34ce139e227f58867f48c59da2ceb9fae19e5b1
jungwon <mac2001035@gmail.com>
2025-04-24 00:18:59 +0900

    CHORE: Spring Web 추가.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,24 @@
 plugins {
+    id("org.springframework.boot") version "3.2.1" // 최신 버전 확인 필요
+    id("io.spring.dependency-management") version "1.1.4"
     id("java")
 }
 
 group = "org.data"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
